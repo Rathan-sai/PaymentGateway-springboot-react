efb9ab32d5f8f80d2713a0aacee78d5ae27c59aa
Rathan Sai <palakonda.rathan@synlabs.io>
2024-09-04 15:54:29 +0530

    start

---
+++ src/main/java/com/programming/tech/PaymentGateway/PaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.programming.tech.PaymentGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayApplication.class, args);
+	}
+
+}
